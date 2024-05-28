f577ae06c3a1f8f0e6a1cef675cc9c16192ab845
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-05-28 16:36:52 +0200

    ADD: import file csv and crate weaponSeeder

---
+++ database/seeders/WeaponSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Weapon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WeaponSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+
+    public function run(): void
+    {
+        $data = $this->getCSVData(__DIR__ . '/items.csv');
+        //    dump($data);
+
+        foreach ($data as $index => $row) {
+            if ($index !== 0) {
+                $weapon = new Weapon();
+                $weapon->name = $row[0];
+                $weapon->category = $row[3];
+                $weapon->weight = $row[4];
+                $weapon->cost = $row[5];
+                $weapon->damage_dice = $row[6];
+                $weapon->save();
+
+            }
+        }
+
+
+    }
+
+    public function getCSVData(string $path)
+    {
+        $data = [];
+
+        $file_stream = fopen($path, 'r');
+
+        if ($file_stream === false) {
+            exit('Error ' . $path);
+        }
+
+        while (($row = fgetcsv($file_stream)) !== false) {
+            $data[] = $row;
+        }
+        fclose($file_stream);
+        return $data;
+    }
+}
