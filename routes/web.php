55e62b01e6f8a217eedc81086458c187682b6276
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-04 15:56:19 +0200

    ADD: installed laravel breeze and pacificdev laravel preset with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\WeaponPageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +15,30 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//  Route::resource('weapons', WeaponPageController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+  //  Route::resource('weapons', WeaponPageController::class);
 
   Route::get('/', [WeaponPageController::class, 'index'] )->name('index');
 
-Route::resource('characters', CharacterController::class);
+  Route::resource('characters', CharacterController::class);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
