8f712e6dd8f54582d1c9eacda9347c094497030a
AntoCic <anto.cic.127@gmail.com>
2024-06-18 00:05:17 +0200

    FIX space header in tyoe.show and type.index

--- resources/views/admin/type/index.blade.php
+++ resources/views/admin/type/index.blade.php
@@ -6,6 +6,9 @@
     <video class="bg-video" src="{{ Vite::asset('resources/img/video/bg-v-5.mp4') }}" autoplay muted loop playsinline
         alt="video bg"></video>
     <div class="container d-flex align-items-center flex-column">
+        @guest
+            <div style="height: 100px"></div>
+        @endguest
         @foreach ($types as $type)
             <a href="{{ route('types.show', $type) }}" class="card text-bg-dark my-4">
                 <img src="{{ Vite::asset($type->img_carosel) }}" class="card-img" alt="...">
--- resources/views/admin/type/show.blade.php
+++ resources/views/admin/type/show.blade.php
@@ -3,29 +3,31 @@
 @section('title', $type->name)
 
 @section('content')
-<img class="bg-video" src="{{ Vite::asset('resources/img/bg/bg-1.png') }}" alt="">
+    <img class="bg-video" src="{{ Vite::asset('resources/img/bg/bg-1.png') }}" alt="">
 
-<div class="container d-flex align-items-center flex-column justify-center py-5">
-    <div class="container py-5">
+    <div class="container d-flex align-items-center flex-column justify-center py-5">
+        <div class="container py-3">
+            @guest
+                <div style="height: 100px"></div>
+            @endguest
+            <div class="card mb-3 bg-dark bg-opacity-75">
+                <img src="{{ Vite::asset($type->img_carosel) }}" class="card-img-top" alt="...">
+                <div class="card-body text-white">
+                    <h5 class="card-title">{{ $type->name }}</h5>
+                    <p class="card-text">{{ $type->description }}</p>
 
-        <div class="card mb-3 bg-dark bg-opacity-75">
-            <img src="{{ Vite::asset($type->img_carosel) }}" class="card-img-top" alt="...">
-            <div class="card-body text-white">
-              <h5 class="card-title">{{$type->name}}</h5>
-              <p class="card-text">{{$type->description}}</p>
-
+                </div>
             </div>
+
+
+
+
+
         </div>
 
 
 
-
-
     </div>
-
-
-
-</div>
 
 
 
