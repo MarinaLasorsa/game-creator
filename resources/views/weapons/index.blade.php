c5675e31f6598c15ae93a23a67ffa4b0cebc69f4
AntoCic <anto.cic.127@gmail.com>
2024-06-17 22:04:30 +0200

    FIX style weapons.index

--- resources/views/weapons/index.blade.php
+++ resources/views/weapons/index.blade.php
@@ -3,38 +3,28 @@
 @section('title', 'Weapons')
 
 @section('content')
-<video class="bg-video" src="{{ Vite::asset('resources/img/video/bg-v-5.mp4') }}" autoplay muted loop playsinline
+    <video class="bg-video" src="{{ Vite::asset('resources/img/video/bg-v-5.mp4') }}" autoplay muted loop playsinline
         alt="video bg"></video>
-<div class="container py-5 text-eb-garamond">
-  <h1 class="text-center text-orange p-3">Armi</h1>
-  <div class="table-responsive card bg-dark">
-    <table class="table table-dark table-hover ">
-      <thead>
-        <tr>
-          <th scope="col">Nome</th>
-          <th scope="col">Categoria</th>
-          <th scope="col">Costo</th>
-          <th scope="col">Peso</th>
-          <th scope="col">Danni</th>
-          
-        </tr>
-  
-        
-      </thead>
-      <tbody>
-      @foreach ($weapons as $weapon )
-      <tr>
-          <th scope="row"><a class="link-orange" href="{{route('admin.weapons.show', $weapon)}}">{{$weapon->name}}</a></th>
-          <td>{{$weapon->category}}</td>
-          <td>{{$weapon->cost}}</td>
-          <td>{{$weapon->weight}}</td>
-          <td>{{$weapon->damage_dice}}</td>
-        </tr>
-      @endforeach
-      </tbody>
-    
-      
-    </table>
-  </div>
-</div>
+    <div class="container py-5 text-eb-garamond">
+        <div class="row justify-content-center">
+            @foreach ($weapons as $weapon)
+                <div class="col-sm-6 col-lg-4 py-3">
+                    <a href="{{ route('admin.weapons.show', $weapon) }}" class="card text-light custom-card h-100">
+                        <img src="{{ Vite::asset($weapon->image) }}" class="card-img-top" alt="...">
+                        <div class="card-body ">
+                            <h5 class="card-title text-center">
+                                {{ $weapon->name }}
+                            </h5>
+                            <!-- <p class="card-text">Categoria :{{ $weapon->category }}</p>
+              <p class="card-text">Costo: {{ $weapon->cost }}</p>
+              <p class="card-text">Peso: {{ $weapon->weight }}</p>
+              <p class="card-text"> Danno : {{ $weapon->damage_dice }}</p>
+              <a href="{{ route('admin.weapons.show', $weapon) }}" class="btn btn-outline-warning">Dettagli arma</a> -->
+                        </div>
+                    </a>
+
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
