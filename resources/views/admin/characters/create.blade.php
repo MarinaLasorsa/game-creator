722e39b02b30a926bc46371dafbe626445ca951f
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-06-18 15:06:48 +0200

    ADDED: some create form style

--- resources/views/admin/characters/create.blade.php
+++ resources/views/admin/characters/create.blade.php
@@ -17,7 +17,7 @@
 
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome</label>
-                        <input type="text" name="name" class="form-control" id="name"
+                        <input type="text" name="name" class="form-control input-weapon" id="name"
                             placeholder="inserisci il nome" value="{{ old('name') }}">
                     </div>
 
@@ -26,18 +26,18 @@
                     <div class="mb-3">
                         <label for="attack" class="form-label">Attacco</label>
                         <input type="number" min="0" max="999" step="1" name="attack"
-                            class="form-control" id="attack" placeholder="inserisci il valore di attacco"
+                            class="form-control input-weapon" id="attack" placeholder="inserisci il valore di attacco"
                             value="{{ old('attack', '1') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="genre" class="form-label">Genere</label>
-                        <select class="form-control" name="genre" id="genre">
+                        <select class="form-control input-weapon" name="genre" id="genre">
 
-                            <option value="">Seleziona Genere</option>
+                            <option class="input-weapon" value="">Seleziona Genere</option>
 
-                            <option @selected('male' == old('genre')) value="male">maschio</option>
-                            <option @selected('female' == old('genre')) value="female">femmina</option>
+                            <option class="input-weapon" @selected('male' == old('genre')) value="male">maschio</option>
+                            <option class="input-weapon" @selected('female' == old('genre')) value="female">femmina</option>
 
 
                         </select>
@@ -45,7 +45,7 @@
 
                     <div class="mb-3">
                         <label for="type_id" class="form-label">Classe</label>
-                        <select class="form-control" name="type_id" id="type_id">
+                        <select class="form-control input-weapon" name="type_id" id="type_id">
                             <option value="">-- Seleziona Classe --</option>
                             @foreach ($types as $type)
                                 <option @selected($type->id == old('type_id')) value="{{ $type->id }}"> {{ $type->name }}
@@ -60,27 +60,27 @@
                     <div class="mb-3">
                         <label for="defence" class="form-label">Difesa</label>
                         <input type="number" min="0" max="999" step="1" name="defence"
-                            class="form-control" id="defence" placeholder="inserisci il valore di difesa"
+                            class="form-control input-weapon" id="defence" placeholder="inserisci il valore di difesa"
                             value="{{ old('defence', '1') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="speed" class="form-label">Velocità</label>
                         <input type="number" min="0" max="999" step="1" name="speed"
-                            class="form-control" id="speed" placeholder="inserisci il valore di velocità"
+                            class="form-control input-weapon" id="speed" placeholder="inserisci il valore di velocità"
                             value="{{ old('speed', '1') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="life" class="form-label">Vita</label>
                         <input type="number" min="0" max="999" step="1" name="life"
-                            class="form-control" id="life" placeholder="inserisci il valore della vita"
+                            class="form-control input-weapon" id="life" placeholder="inserisci il valore della vita"
                             value="{{ old('life', '1') }}">
                     </div>
 
                     <div class="my-3">
-                        <div class="form-label">Armi</div>
-                        <div class="d-flex flex-wrap gap-3">
+                        <div class="form-label ">Armi</div>
+                        <div class="row">
                             @foreach ($weapons as $weapon)
                                 @php
                                     $arr_weapons = old('weapons', null);
@@ -91,11 +91,14 @@
                                     //     $value_weapon = '0';
                                     // }
                                 @endphp
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text"
-                                        for="weapons-{{ $weapon->id }}">{{ $weapon->name }}</label>
-                                    <input type="number" class="form-control" name="weapons[]"
-                                        id="weapons-{{ $weapon->id }}" value="{{$arr_weapons != null ? $arr_weapons[$loop->index] : '0'}}">
+                                <div class="col-3">
+                                    <div class="input-group  mb-3">
+                                        <label class="input-group-text input  "
+                                            for="weapons-{{ $weapon->id }}">{{ $weapon->name }}</label>
+                                        <input type="number" class="form-control input-weapon" name="weapons[]"
+                                            id="weapons-{{ $weapon->id }}" value="{{$arr_weapons != null ? $arr_weapons[$loop->index] : '0'}}">
+                                    </div>
+
                                 </div>
                             @endforeach
                         </div>
@@ -106,7 +109,7 @@
 
                     <div class="mb-3">
                         <label for="description" class="form-label">Descrizione</label>
-                        <textarea class="form-control" name="description" id="description" rows="3"
+                        <textarea class="form-control  input-weapon" name="description" id="description" rows="3"
                             placeholder="Descrizione del personaggio">{{ old('description') }}</textarea>
                     </div>
 
